cac87a084cde3fd4b6ed8ce0907b622a1e385d2d
facundo torres <tfacundo108@gmail.com>
2022-11-29 21:48:29 -0300

    adding for loop

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ let strgTotal = "Total with interests: ";
 let ivaEl = document.querySelector("#iva");
 document.querySelector("#iva-button").style.visibility = 'hidden';
 const iva = Number(1.21);
-let price = 0
-let interest = 0
-let operation = 0
+let price = 0;
+let interest = 0;
+let operation = 0;
+let count = 0;
 
 function calculate(){
     price = parseInt(document.querySelector("#price").value);
@@ -18,19 +19,28 @@ function cond(){
     if (isNaN(price)){
         document.querySelector("#error1").textContent = "Please enter a valid number"
         total.textContent = strgTotal
+        count += 1
     } else if (isNaN(interest)){
         document.querySelector("#error2").textContent = "Please enter a valid interest rate"
         total.textContent = strgTotal
+        count += 1
     } else if (isNaN(interest) && isNan(price)){
         document.querySelector("#error2").textContent = "Please enter a valid interest rate"
         total.textContent = strgTotal
         document.querySelector("#error1").textContent = "Please enter a valid number"
         total.textContent = strgTotal
+        count += 1
     } else{
         total.textContent = strgTotal + operation;
         document.querySelector("#error1").textContent = "";
         document.querySelector("#error2").textContent = "";
         document.querySelector("#iva-button").style.visibility = 'visible';
+    };
+    // Cuando el usuario llega a 10 veces sin poner un numero y querer hacer el calculo, salta una alerta
+    for (i = count; i > 10; i){
+        if (i = 10){
+            alert("You have entered too many errors. Please refresh the website.")
+        }
     }
 }
 
@@ -48,5 +58,5 @@ function newEntry(){
 function calcIva () {
     calculate()
     const ivaOp = operation * iva;
-    document.querySelector("#iva-el").textContent = "Total with IVA: " + ivaOp;
+    document.querySelector("#iva-el").textContent = "Total with IVA: " + ivaOp.toFixed(2);
 }
